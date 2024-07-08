7acf80bcd53b1cf2c29b0eaa3ecc7f2dd7f84005
Rizanriz <rizanr7777@gmail.com>
2024-07-09 01:30:54 +0530

    ongoing commit

---
+++ components/main/Encryption.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const Encryption = () => {
+  return (
+    <div className='flex flex-row relative items-center justify-center w-full h-full'>
+        <div className="absolute w-auto h-auto top-0 z-[5]">
+            <motion.div>
+                
+            </motion.div>
+        </div>
+    </div>
+  )
+}
+
+export default Encryption

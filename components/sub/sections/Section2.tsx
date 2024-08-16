123305f10f859d4793b94c6293ddcd31c46624d1
Rizanriz <rizanr7777@gmail.com>
2024-08-17 04:24:31 +0530

    ongoing commit

--- components/sub/sections/Section2.tsx
+++ components/sub/sections/Section2.tsx
@@ -6,13 +6,13 @@ const Section2 = () => {
     <>
        <section id="about">
              <div className='about'>
-                <div className="about_info" >
-                    <h3 > EXPIRENCE</h3>
+                <div className="about_info mt-10 sm:mt-0" >
+                    <h3 > EXPIRIENCE</h3>
                     <p> For the past seven months, I have been a part of Luminar Technolab, where I have honed my skills in the MERN stack . During this time, I have worked on various projects, enhancing my expertise in building responsive and dynamic web applications.
                        My experience at Luminar has equipped me with hands-on knowledge in full-stack development, solidifying both my technical and problem-solving abilities.</p>
                 </div>
-                <div className="about_info" >
-                    <h3 >AREAS OF EXPERTISE:</h3>
+                <div className="about_info pl-0 sm:pl-20">
+                    <h3>AREAS OF EXPERTISE:</h3>
                     <h5> Languages : <span>HTML , CSS , Javascript , Nodejs </span></h5>
                     <h5> Frameworks : <span>Express.js , Tailwind , Bootstrap , NEXT.js</span></h5>
                     <h5> Dev Tools : <span>VS code , Figma , Postman , Github</span></h5>

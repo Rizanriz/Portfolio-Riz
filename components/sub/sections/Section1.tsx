f2892209ef0010cb8664b7e95a8e3c6d0e2050b8
Rizanriz <rizanr7777@gmail.com>
2024-08-28 13:55:51 +0530

    ongoing commit

--- components/sub/sections/Section1.tsx
+++ components/sub/sections/Section1.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import './Section1.css'
 import Image from 'next/image'
 
 function Section1() {
   return (
     <>
-       <section id="about">
-             <div className='about'>
-                <div className="about_img" >
-                <Image
-                    src="/hero.jpeg"
-                    alt="work icon" height={650} width={650}
-                    />
-                </div>
-                <div className="about_info" >
-                    <h3 >About Me</h3>
-                    <h3>I&apos;m a <span>FULL STACK </span>Developer</h3>
-                    <p> Full Stack MERN Developer, I specialize in building responsive, dynamic, and scalable web applications using MongoDB, Express.js, React, and Node.js. With a strong foundation in JavaScript,
-                       I am adept at developing both client-side and server-side components, ensuring seamless integration and performance.</p>
-                  </div>
-            </div>
-          </section> 
+      <section id="about" className="w-full flex items-center justify-around py-20 px-6 gap-5 text-white z-10">
+        <div className="relative w-100 h-125">
+          <Image
+            src="/hero.jpeg"
+            alt="work icon"
+            height={500} width={500}
+            className="relative border-2 border-purple-500"
+          />
+          <div className="absolute top-5 left-5 w-full h-full border-2 border-purple-500 z-1"></div>
+        </div>
+        <div className="w-1/2">
+          <h3 className="mb-5 text-5xl font-light">
+            About Me
+          </h3>
+          <h3 className="text-5xl font-light mb-5">
+            I'm a <span className="font-medium border-b-2 border-pink-500">FULL STACK</span> Developer
+          </h3>
+          <p className="text-xl">
+            Full Stack MERN Developer, I specialize in building responsive, dynamic, and scalable web applications using MongoDB, Express.js, React, and Node.js. With a strong foundation in JavaScript, I am adept at developing both client-side and server-side components, ensuring seamless integration and performance.
+          </p>
+        </div>
+      </section>
     </>
   )
 }
--- components/sub/sections/Section2.tsx
+++ components/sub/sections/Section2.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import './Section1.css'
 
 const Section2 = () => {
   return (
     <>
-       <section id="about">
-             <div className='about'>
-                <div className="about_info" >
-                    <h3 > EXPIRIENCE</h3>
-                    <p> For the past seven months, I have been a part of Luminar Technolab, where I have honed my skills in the MERN stack . During this time, I have worked on various projects, enhancing my expertise in building responsive and dynamic web applications.
-                       My experience at Luminar has equipped me with hands-on knowledge in full-stack development, solidifying both my technical and problem-solving abilities.</p>
-                </div>
-                <div className="about_info pl-0 sm:pl-20">
-                    <h3>AREAS OF EXPERTISE:</h3>
-                    <h5> Languages : <span>HTML , CSS , Javascript , Nodejs </span></h5>
-                    <h5> Frameworks : <span>Express.js , Tailwind , Bootstrap , NEXT.js</span></h5>
-                    <h5> Dev Tools : <span>VS code , Figma , Postman , Github</span></h5>
-                    <h5> Libraries : <span>React.js , Redux Toolkit , Mongoose, Axios ,  jsonwebtoken , bcryptjs , multer </span></h5>
-                    <h5> Familiar : <span>Angular , Material UI ,Framer Motion , GSAP , Three.js</span></h5>
-                </div>
-            </div>
-          </section> 
+      <section id="about" className="w-full flex flex-col items-center justify-around py-20 px-6 gap-5 text-white z-10">
+        <div className="w-full text-center sm:text-left sm:pl-20 mt-20">
+          <h3 className="mb-5 text-5xl font-light">
+            EXPERIENCE
+          </h3>
+          <p className="text-xl">
+            For the past seven months, I have been a part of Luminar Technolab, where I have honed my skills in the MERN stack. During this time, I have worked on various projects, enhancing my expertise in building responsive and dynamic web applications. My experience at Luminar has equipped me with hands-on knowledge in full-stack development, solidifying both my technical and problem-solving abilities.
+          </p>
+        </div>
+        <div className="w-full text-center sm:text-left sm:pl-20">
+          <h3 className="mb-5 text-5xl font-light">
+            AREAS OF EXPERTISE:
+          </h3>
+          <h5 className="text-2xl text-gray-500 mb-5">
+            Languages: <span className="text-white italic">HTML, CSS, JavaScript, Node.js</span>
+          </h5>
+          <h5 className="text-2xl text-gray-500 mb-5">
+            Frameworks: <span className="text-white italic">Express.js, Tailwind, Bootstrap, NEXT.js</span>
+          </h5>
+          <h5 className="text-2xl text-gray-500 mb-5">
+            Dev Tools: <span className="text-white italic">VS Code, Figma, Postman, GitHub</span>
+          </h5>
+          <h5 className="text-2xl text-gray-500 mb-5">
+            Libraries: <span className="text-white italic">React.js, Redux Toolkit, Mongoose, Axios, jsonwebtoken, bcryptjs, multer</span>
+          </h5>
+          <h5 className="text-2xl text-gray-500 mb-5">
+            Familiar: <span className="text-white italic">Angular, Material UI, Framer Motion, GSAP, Three.js</span>
+          </h5>
+        </div>
+      </section>
     </>
   )
 }
